d3f024346b210f93cad224451b5b458433c339f6
cbrumbau <cbrumbau@users.noreply.github.com>
2025-12-16 13:47:15 -0800

    Add new options and clean up cli script
    Clean up argparse options for files and folders
    
    Change logging to use adba logging
    
    Add option to use credentials ini file for aniDB login
    
    Clean up print statements and exception messages
    
    Add option to wait on user input before exiting the script

--- adbacli.py
+++ adbacli.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import inspect
-import logging
+import configparser
 import os
 import sys
 import adba
-import pprint
 
 stateToUDP = dict(unknown=0, hdd=1, cd=2, deleted=3)
 viewedToUDP = dict(unwatched=0, watched=1)
@@ -16,45 +14,56 @@ fileTypes = '.avi,.mp4,.mkv,.ogm'
 parser = argparse.ArgumentParser()
 parser.add_argument('command', choices=['hash', 'mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields', 'listfields', 'logout'],
 help='Command to execute. hash: Hash the file and print its ed2k hash. mylistadd: Hash the file and add it to AniDB MyList. If the file was there, it will be updated. mylistdel: Hash the file and delete it from AniDB MyList. getfields: Hash the file and retrieve requested fields for the hashed file. listfields: Lists all the available fields that can be requested from AniDB.')
+parser.add_argument('file', nargs='*', default=[],
+help='All file(s) and/or folder(s) to be processed.')
 parser.add_argument('--file-types', default=fileTypes,
 help='A comma delmited list of file types to be included when searching directories. Default: ' + fileTypes)
-parser.add_argument('--out-file', action='store', default=None,
+parser.add_argument('--out-file',
 help='Write output to specified file instead of STDOUT.')
-parser.add_argument('-u', '--username', action='store', default=None,
+parser.add_argument('-u', '--username',
 help='User name needed to communicate with AniDB.')
-parser.add_argument('-p', '--password', action='store', default=None,
+parser.add_argument('-p', '--password',
 help='Password needed to communicate with AniDB.')
+parser.add_argument('-l', '--login',
+help='Login credentials ini file to login to AniDB.')
 parser.add_argument('--state', choices=['unknown', 'hdd', 'cd', 'deleted'], default='hdd',
 help='Sets the file state to unknown/hdd/cd/deleted. Default: hdd')
 parser.add_argument('--watched', action='store_true',
 help='Marks the file as watched.')
 parser.add_argument('--unwatched', action='store_true',
 help='Marks the file as unwatched.')
-parser.add_argument('--source', action='store', default=None,
+parser.add_argument('--source',
 help='Sets the file source (any string).')
-parser.add_argument('--storage', action='store', default=None,
+parser.add_argument('--storage',
 help='Sets file storage (any string).')
-parser.add_argument('--other', action='store', default=None,
+parser.add_argument('--other',
 help='Sets other remarks (any string).')
-parser.add_argument('--fields', action='store', default=None,
+parser.add_argument('--fields',
 help='A comma delimited list of fields requested from AniDB.')
+parser.add_argument('--wait-input', action='store_true',
+help='Wait for user input before exiting the script.')
 parser.add_argument('--fast-command-delay', action='store_true',
 help='Specify the command delay to wait for 2.1 seconds as opposed to the default of 4.1 seconds.')
-parser.add_argument('files', nargs='*', default=[],
-help='All files and/or folders to be processed.')
+parser.add_argument('--logging', action='store_true',
+help='Enable debug level logging to file.')
 
-args, otherArgs = parser.parse_known_args()
+args = parser.parse_args()
 
-fileParser = argparse.ArgumentParser()
-fileParser.add_argument('files', nargs='*', default=[],
-help='All files and/or folders to be processed.')
+if args.login:
+	# Extract the login credentials
+	config = configparser.ConfigParser()
+	try:
+		config.read(args.login)
+		args.username = config['DEFAULT']['user']
+		args.password = config['DEFAULT']['password']
+	except Exception as e:
+		print('Exception:', e)
+		input("Press Enter to continue")
+		sys.exit(1)
 
-fileArgs = fileParser.parse_args(otherArgs)
-
-allFiles = args.files + fileArgs.files
-
-# Start logging
-FileListener = adba.StartLogging()
+if args.logging:
+	# Start logging
+	FileListener = adba.StartLogging()
 
 if args.out_file:
 	sys.stdout = open(args.out_file, 'w', encoding='UTF-8')
@@ -63,16 +72,21 @@ if args.out_file:
 if args.command == 'logout':
 	connection = adba.Connection(commandDelay=2.1)
 	connection.logout()
+	input("Press Enter to continue")
 	sys.exit(0)
 
 # Check if fields are required
 if args.command in ['mylistaddwithfields', 'getfields']:
 	if not args.fields:
 		print("Fields to retrieve are required for " + args.command + ".")
+		input("Press Enter to continue")
 		sys.exit(0)
 
 # Convert state to UDP
-args.state = stateToUDP[args.state]
+if args.command == 'mylistdel':
+	args.state = stateToUDP['deleted']
+else:
+	args.state = stateToUDP[args.state]
 
 # Convert watched to UDP
 if args.watched:
@@ -85,7 +99,7 @@ else:
 # Parse positional arguments passed in and convert all to files
 fileList = list()
 validExtensions = args.file_types.lower().split(',')
-for entry in list(allFiles):
+for entry in list(args.file):
 	if os.path.isfile(entry):
 		if any(os.path.splitext(entry)[1].lower() == valid for valid in validExtensions):
 			fileList.append(entry)
@@ -99,12 +113,14 @@ for entry in list(allFiles):
 if args.command in ['hash', 'mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields']:
 	if len(fileList) == 0:
 		print("Files and/or directories containing valid files are required for " + args.command + ".")
+		input("Press Enter to continue")
 		sys.exit(0)
 
 # Check if login is required and create connection if have login credentials
 if args.command in ['mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields']:
 	if not args.username or not args.password:
 		print("User and password required for " + args.command + ".")
+		input("Press Enter to continue")
 		sys.exit(0)
 	if args.fast_command_delay:
 		connection = adba.Connection(commandDelay=2.1)
@@ -113,8 +129,7 @@ if args.command in ['mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields'
 	try:
 		connection.auth(args.username, args.password)
 	except Exception as e:
-		print('Exception: %s', e)
-		logging.debug('Exception Occured: '+str(e))
+		print('Exception:', e)
 
 # Execute command
 if args.command == 'hash':
@@ -122,10 +137,6 @@ if args.command == 'hash':
 	for thisFile in fileList:
 		thisED2K = adba.aniDBfileInfo.get_ED2K(thisFile, forceHash=True)
 		print(thisED2K)
-	if args.out_file:
-		sys.stdout.close()
-	adba.StopLogging(FileListener)
-	sys.exit(0)
 elif args.command == 'mylistadd':
 	# First try to add the file, then try to edit
 	if connection.authed():
@@ -133,20 +144,17 @@ elif args.command == 'mylistadd':
 			try:
 				episode = adba.Episode(connection, filePath=thisFile)
 			except Exception as e:
-				print('Exception: %s', e)
-				logging.debug('Exception Occured: '+str(e))
+				print('Exception:', e)
 				continue
 			try:
 				episode.edit_to_mylist(state=args.state, viewed=viewed, source=args.source, storage=args.storage, other=args.other)
-				print(thisFile + " successfully edited in AniDB MyList.")
-			except Exception as e:
-				logging.debug('Exception Occured: '+str(e))
+				print(os.path.basename(thisFile) + " successfully edited in AniDB MyList.")
+			except:
 				try:
 					episode.add_to_mylist(state=args.state, viewed=viewed, source=args.source, storage=args.storage, other=args.other)
-					print(thisFile + " successfully added to AniDB MyList.")
+					print(os.path.basename(thisFile) + " successfully added to AniDB MyList.")
 				except Exception as e:
-					print('Exception: %s', e)
-					logging.debug('Exception Occured: '+str(e))
+					print('Exception:', e)
 					continue
 elif args.command == 'mylistdel':
 	# Delete the file
@@ -155,16 +163,18 @@ elif args.command == 'mylistdel':
 			try:
 				episode = adba.Episode(connection, filePath=thisFile)
 			except Exception as e:
-				print('Exception: %s', e)
-				logging.debug('Exception Occured: '+str(e))
+				print('Exception:', e)
 				continue
 			try:
-				episode.delete_from_mylist()
-				print(thisFile + " successfully removed from AniDB MyList.")
+				episode.edit_to_mylist(state=args.state, viewed=viewed, source=args.source, storage=args.storage, other=args.other)
+				print(os.path.basename(thisFile) + " successfully edited as deleted from AniDB MyList.")
 			except Exception as e:
-				print('Exception: %s', e)
-				logging.debug('Exception Occured: '+str(e))
-				continue
+				try:
+					episode.add_to_mylist(state=args.state, viewed=viewed, source=args.source, storage=args.storage, other=args.other)
+					print(os.path.basename(thisFile) + " successfully added as deleted to AniDB MyList.")
+				except Exception as e:
+					print('Exception:', e)
+					continue
 elif args.command == 'mylistaddwithfields':
 	# Parse requested field(s)
 	requestedFields = list(args.fields.lower().split(','))
@@ -180,20 +190,17 @@ elif args.command == 'mylistaddwithfields':
 			try:
 				episode = adba.Episode(connection, filePath=thisFile, load=True, paramsF=requestF, paramsA=requestA)
 			except Exception as e:
-				print('Exception: %s', e)
-				logging.debug('Exception Occured: '+str(e))
+				print('Exception:', e)
 				continue
 			try:
 				episode.edit_to_mylist(state=args.state, viewed=viewed, source=args.source, storage=args.storage, other=args.other)
-				print(thisFile + " successfully edited in AniDB MyList.")
-			except Exception as e:
-				logging.debug('Exception Occured: '+str(e))
+				print(os.path.basename(thisFile) + " successfully edited in AniDB MyList.")
+			except:
 				try:
 					episode.add_to_mylist(state=args.state, viewed=viewed, source=args.source, storage=args.storage, other=args.other)
-					print(thisFile + " successfully added to AniDB MyList.")
+					print(os.path.basename(thisFile) + " successfully added to AniDB MyList.")
 				except Exception as e:
-					print('Exception: %s', e)
-					logging.debug('Exception Occured: '+str(e))
+					print('Exception:', e)
 					continue
 			print("filename\t" + thisFile)
 			for field in requestedFields:
@@ -214,8 +221,7 @@ elif args.command == 'getfields':
 			try:
 				episode = adba.Episode(connection, filePath=thisFile, load=True, paramsF=requestF, paramsA=requestA)
 			except Exception as e:
-				print('Exception: %s', e)
-				logging.debug('Exception Occured: '+str(e))
+				print('Exception:', e)
 				continue
 			print("filename\t" + thisFile)
 			for field in requestedFields:
@@ -233,8 +239,11 @@ elif args.command == 'listfields':
 connection.stayloggedin()
 
 if args.out_file:
-        sys.stdout.close()
+	sys.stdout.close()
 
-adba.StopLogging(FileListener)
+if (args.logging):
+	adba.StopLogging(FileListener)
 
+if args.wait_input:
+	input("Press Enter to continue")
 sys.exit(0)
